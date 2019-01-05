df51717eef9a2dda0af9df462aa8774d675b2398
Alan Koval <alank399@gmail.com>
2019-01-05 16:24:55 -0500

    Add linux build, aparently other things too.

--- Build/build_linux.js
+++ Build/build_linux.js
@@ -1,19 +1,19 @@
 const files = require('fs');
-const exec = require('child_process').execSync;
+const shell = require('shelljs');
 
 // Remove result directory, then add it back (remove all the files)
 // /s is for removing all sub-elements, /q is for quiet (no confirmation)
 if(files.existsSync('result')){
-	exec('rmdir /s /q result');
+	shell.exec('rm -r result');
 }
-exec('mkdir result');
+shell.exec('mkdir result');
 
 // copy entire ace-src-min and img folders
 for(const name of ['ace-src-min', 'img']){
-	exec(`echo d | xcopy /s /q /r /y "../${name}" "result/${name}"`);
+    shell.exec(`cp -r "../${name}" "result/${name}"`);
 }
 
 // copy pixel.js, index.html and styles.css
-exec('copy "..\\pixel.js" "result\\pixel.js"');
-exec('copy "..\\index.html" "result\\index.html"');
-exec('copy "..\\styles.css" "result\\styles.css"');
+shell.exec('cp ../pixel.js result/');
+shell.exec('cp ../index.html result/');
+shell.exec('cp ../styles.css result/');
